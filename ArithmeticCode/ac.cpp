c4007c61e7d31b583062740cd83407516fd87712
luweir <463047622@qq.com>
2021-10-26 07:49:13 +0800

    10/26遇到溢出问题

--- ArithmeticCode/ac.cpp
+++ ArithmeticCode/ac.cpp
@@ -14,6 +14,7 @@ void quit(const char *message = 0)
 class ArithmeticCode
 {
 private:
+    int totalLength;
     map<unsigned char, int> statistic;
     map<unsigned char, pair<double, double>> probabilityTable;
 
@@ -24,6 +25,7 @@ public:
         int c;
         fseek(in, 0, SEEK_END);
         int size = ftell(in);
+        totalLength = size;
         if (size < 0 || size >= 0x7FFFFFFF)
             quit("input file too big");
         fseek(in, 0, SEEK_SET);
@@ -33,8 +35,9 @@ public:
         }
     }
     // 生成概率
-    void GenerateProbability(int size)
+    void GenerateProbability()
     {
+        int size = totalLength;
         double count = 0;
         for (const auto &p : statistic)
         {
@@ -47,17 +50,26 @@ public:
     {
         fseek(in, 0, SEEK_SET);
         double low = 0;
-        double high = 1;
+        double high = 100;
         double newLow, newHigh;
         int c = 0;
-        while ((c = getc(in) != EOF))
+        while ((c = getc(in)) != EOF)
         {
             newLow = low + (high - low) * probabilityTable[c].first;
             newHigh = low + (high - low) * probabilityTable[c].second;
             low = newLow;
             high = newHigh;
         }
+        // 算术编码的溢出问题难以解决
+        cout << (low == high) << endl;
+        cout << setprecision(15) << low << endl
+             << setprecision(15) << high << endl;
         low = (low + high) / 2.0;
+
+        char *str = new char[50];
+        memset(str, 0, 20);
+        sprintf(str, "%.20f", low); //将a格式化输出到str中，取小数点后7位
+        cout << str << endl;
         unsigned long long num = (unsigned long long)low;
         // 把小数输出到文件
         putc(num >> 56, out);
@@ -69,15 +81,39 @@ public:
         putc(num >> 8, out);
         putc(low, out);
         // 把统计词频输出到文件
-        
+        for (auto pair : statistic)
+        {
+            // 字符0~255，一个字节直接写入
+            putc((int)pair.first, out);
+            // 次数的话，由于一个文件随随便便上万个重复字符，所以这里用四个字节来存频率
+            // 相当于以（1个字节的字符，4个字节的频率）将每个不同的字符频率都写进去：1个字节*10^9=1G，基本够用
+            // 尤其对于大文件
+            putc((int)(pair.second >> 24), out);
+            putc((int)(pair.second >> 16), out);
+            putc((int)(pair.second >> 8), out);
+            putc((int)(pair.second), out);
+        }
+    }
+    void CompressFile(FILE *in, FILE *out)
+    {
+        WordFrequencyStatistics(in);
+        GenerateProbability();
+        Encode(in, out);
     }
 };
 
 int main()
 {
     int argc = 4;
-    char argv[][50] = {"", "", "", ""};
-    long long a = 0;
-    cout << sizeof(a) << endl;
+    char argv[][50] = {"ac", "c", "./project/DataCompression/ArithmeticCode/data.txt", "./project/DataCompression/ArithmeticCode/data.ac"};
+    ArithmeticCode *ac = new ArithmeticCode();
+    FILE *in = fopen(argv[2], "rb");
+    if (!in)
+    {
+        perror(argv[0]);
+        exit(1);
+    }
+    FILE *out = fopen(argv[3], "wb");
+    ac->CompressFile(in, out);
     return 0;
 }
