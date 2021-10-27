752e94238ca03d714c8189ba50bc0f6935df535b
luweir <463047622@qq.com>
2021-10-27 17:03:39 +0800

    Solve the problem of overflow, arithmetic coding can achieve compression

--- ArithmeticCode/ac.cpp
+++ ArithmeticCode/ac.cpp
@@ -1,7 +1,11 @@
 #include <iostream>
 #include <assert.h>
 #include <bits/stdc++.h>
+
 using namespace std;
+
+using std::uint32_t;
+using std::uint64_t;
 
 // Error handler: print message if any, and exit
 void quit(const char *message = 0)
@@ -11,94 +15,188 @@ void quit(const char *message = 0)
     exit(1);
 }
 
-class ArithmeticCode
+////////////////////////////////////// FrequencyTable ////////////////////////////////////
+
+class FrequencyTable
+{
+public:
+    vector<uint32_t> statistic;  // 频率表
+    vector<uint32_t> cumulative; // 累计频率表
+    uint32_t total;
+    FrequencyTable()
+    {
+        statistic = vector<uint32_t>(257, 0);
+        int size = statistic.size();
+        cumulative.reserve(size + 1);
+        total = 0;
+        initCumulative();
+        total = getHigh(size - 1);
+    }
+    // 增加对应字符的频率
+    void IncreaseFrequency(uint32_t symbol)
+    {
+        total++;
+        statistic[symbol]++;
+        cumulative.clear();
+    }
+    // 获得symbol的频率下界
+    uint32_t getLow(uint32_t symbol)
+    {
+        initCumulative();
+        return cumulative[symbol];
+    }
+    // 获得symbol的频率上届
+    uint32_t getHigh(uint32_t symbol)
+    {
+        initCumulative();
+        return cumulative[symbol + 1];
+    }
+    // 向文件out写入频率表
+    void WriteFrequencyTable(FILE *out)
+    {
+        for (int i = 0; i < 256; i++)
+        {
+            uint32_t tar = statistic[i];
+            fputc(tar >> 24, out);
+            fputc(tar >> 16, out);
+            fputc(tar >> 8, out);
+            fputc(tar, out);
+        }
+    }
+    void initCumulative()
+    {
+        if (!cumulative.empty())
+        {
+            return;
+        }
+        uint32_t sum = 0;
+        cumulative.push_back(sum);
+        for (uint32_t freq : statistic)
+        {
+            sum += freq;
+            cumulative.push_back(sum);
+        }
+        if (sum != total)
+            quit("the sum is not equal total");
+    }
+};
+
+////////////////////////////////////// BitOutPut ////////////////////////////////////
+class BitOutPut
+{
+public:
+    FILE *out;
+    int currentByte;
+    int numBitsFilled;
+    BitOutPut(FILE *&outt)
+    {
+        out = outt;
+        currentByte = 0;
+        numBitsFilled = 0;
+    }
+    // 保存位b，凑够8位后组成unsigned char 写入文件
+    void writeBit(int b)
+    {
+        if (b != 0 && b != 1)
+            quit("the write value of Bitoutput must be 0 or 1");
+        currentByte = (currentByte << 1) | b;
+        numBitsFilled++;
+        if (numBitsFilled == 8)
+        {
+            putc(currentByte, out);
+            currentByte = 0;
+            numBitsFilled = 0;
+        }
+    }
+    void finish()
+    {
+        while (numBitsFilled != 0)
+            writeBit(0);
+    }
+};
+////////////////////////////////////// ArithmeticEnCoder ////////////////////////////////////
+class ArithmeticEnCoder
 {
 private:
-    int totalLength;
-    map<unsigned char, int> statistic;
-    map<unsigned char, pair<double, double>> probabilityTable;
+    int numStateBits;      // 状态位数量
+    uint32_t numUnderflow; // 下溢位数
+    uint64_t fullRange;    // 总范围
+    uint64_t halfRange;    // 半范围
+    uint64_t quarterRange; // 四分范围
+    uint64_t minimumRange;
+    uint64_t maximumTotal;
+    uint64_t stateMask; // 状态掩码
+    uint64_t low;       // 下界
+    uint64_t high;      // 上届
+    BitOutPut *output;
 
 public:
-    // 统计词频
-    void WordFrequencyStatistics(FILE *in)
+    ArithmeticEnCoder(int numBits, BitOutPut *&bout)
     {
-        int c;
-        fseek(in, 0, SEEK_END);
-        int size = ftell(in);
-        totalLength = size;
-        if (size < 0 || size >= 0x7FFFFFFF)
-            quit("input file too big");
-        fseek(in, 0, SEEK_SET);
-        while ((c = getc(in)) != EOF)
-        {
-            statistic[c]++;
-        }
+        numStateBits = numBits;
+        fullRange = static_cast<decltype(fullRange)>(1) << numStateBits;
+        halfRange = fullRange >> 1;                                                                         // halfRange表示最高位为1，其他位为0，方便后面shift
+        quarterRange = halfRange >> 1;                                                                      // Can be zero ???为什么要设立这些
+        minimumRange = quarterRange + 2;                                                                    // At least 2 ???
+        maximumTotal = std::min(std::numeric_limits<decltype(fullRange)>::max() / fullRange, minimumRange); // ???
+        stateMask = fullRange - 1;                                                                          // 即stateMask的numStateBits个位全为1
+        low = 0;
+        high = stateMask;
+        output = bout;
+        numUnderflow = 0;
     }
-    // 生成概率
-    void GenerateProbability()
+    // 结果更新此算术编码器的代码范围（低和高）
+    void update(FrequencyTable *&freqs, uint32_t symbol)
     {
-        int size = totalLength;
-        double count = 0;
-        for (const auto &p : statistic)
-        {
-            probabilityTable[p.first] = pair<double, double>(count / (double)size, (count + p.second) / (double)size);
-            count += p.second;
-        }
-    }
-    // 编码
-    void Encode(FILE *in, FILE *out)
-    {
-        fseek(in, 0, SEEK_SET);
-        double low = 0;
-        double high = 100;
-        double newLow, newHigh;
-        int c = 0;
-        while ((c = getc(in)) != EOF)
-        {
-            newLow = low + (high - low) * probabilityTable[c].first;
-            newHigh = low + (high - low) * probabilityTable[c].second;
-            low = newLow;
-            high = newHigh;
-        }
-        // 算术编码的溢出问题难以解决
-        cout << (low == high) << endl;
-        cout << setprecision(15) << low << endl
-             << setprecision(15) << high << endl;
-        low = (low + high) / 2.0;
+        uint64_t range = high - low + 1;
+        uint32_t total = freqs->total;
+        uint32_t symLow = freqs->getLow(symbol);
+        uint32_t symHigh = freqs->getHigh(symbol);
+        // Update range
+        uint64_t newLow = low + symLow * range / total;
+        uint64_t newHigh = low + symHigh * range / total - 1;
+        low = newLow;
+        high = newHigh;
 
-        char *str = new char[50];
-        memset(str, 0, 20);
-        sprintf(str, "%.20f", low); //将a格式化输出到str中，取小数点后7位
-        cout << str << endl;
-        unsigned long long num = (unsigned long long)low;
-        // 把小数输出到文件
-        putc(num >> 56, out);
-        putc(num >> 48, out);
-        putc(num >> 40, out);
-        putc(num >> 32, out);
-        putc(num >> 24, out);
-        putc(num >> 16, out);
-        putc(num >> 8, out);
-        putc(low, out);
-        // 把统计词频输出到文件
-        for (auto pair : statistic)
+        //如果low和high具有相同的最高位值，将它们移出
+        while (((low ^ high) & halfRange) == 0)
         {
-            // 字符0~255，一个字节直接写入
-            putc((int)pair.first, out);
-            // 次数的话，由于一个文件随随便便上万个重复字符，所以这里用四个字节来存频率
-            // 相当于以（1个字节的字符，4个字节的频率）将每个不同的字符频率都写进去：1个字节*10^9=1G，基本够用
-            // 尤其对于大文件
-            putc((int)(pair.second >> 24), out);
-            putc((int)(pair.second >> 16), out);
-            putc((int)(pair.second >> 8), out);
-            putc((int)(pair.second), out);
+            shift();
+            low = ((low << 1) & stateMask);       // 删除 low 的MSD（MSD 代表数字中的最高有效数字或最左边的数字）并将新的 0 移到low
+            high = ((high << 1) & stateMask) | 1; // 删除高的 MSD 并将新的 1 移到high
+        }
+
+        // 现在low的最高位必须为 0，high的最高位必须为 1
+
+        // 当low的高两位是 01 和high是 10 时，删除两者的第二高位;可能是二者接近，后面有溢出风险，所以要做变换；
+        while ((low & ~high & quarterRange) != 0)
+        {
+            underflow();
+            low = (low << 1) ^ halfRange;
+            high = ((high ^ halfRange) << 1) | halfRange | 1;
         }
     }
-    void CompressFile(FILE *in, FILE *out)
+    //根据给定的频率表对给定的符号进行编码,还更新此算术编码器的状态并可能写出一些位
+    void write(FrequencyTable *&freqs, uint32_t symbol)
     {
-        WordFrequencyStatistics(in);
-        GenerateProbability();
-        Encode(in, out);
+        update(freqs, symbol);
+    }
+    // 调用以处理 low 和 high 的最高位相等时的情况
+    void shift()
+    {
+        int bit = (int)(low >> (numStateBits - 1));
+        output->writeBit(bit);
+        for (; numUnderflow > 0; numUnderflow--)
+            output->writeBit(bit ^ 1);
+    }
+    void finish()
+    {
+        output->writeBit(1);
+    }
+    // 当 low=01(...) 和 high=10(...) 时调用处理这种情况
+    void underflow()
+    {
+        numUnderflow++;
     }
 };
 
@@ -106,7 +204,6 @@ int main()
 {
     int argc = 4;
     char argv[][50] = {"ac", "c", "./project/DataCompression/ArithmeticCode/data.txt", "./project/DataCompression/ArithmeticCode/data.ac"};
-    ArithmeticCode *ac = new ArithmeticCode();
     FILE *in = fopen(argv[2], "rb");
     if (!in)
     {
@@ -114,6 +211,28 @@ int main()
         exit(1);
     }
     FILE *out = fopen(argv[3], "wb");
-    ac->CompressFile(in, out);
+
+    // 生成频率表
+    FrequencyTable *freqs = new FrequencyTable();
+    int symbol = 0;
+    while ((symbol = getc(in)) != EOF)
+    {
+        freqs->IncreaseFrequency((uint32_t)symbol);
+    }
+    fseek(in, 0, SEEK_SET);
+
+    // 向out写入频率表
+    freqs->WriteFrequencyTable(out);
+    BitOutPut *bout = new BitOutPut(out);
+
+    // 编码压缩
+    ArithmeticEnCoder *encoder = new ArithmeticEnCoder(32, bout);
+    while ((symbol = getc(in)) != EOF)
+    {
+        encoder->write(freqs, static_cast<uint32_t>(symbol));
+    }
+    encoder->write(freqs, 256); // EOF
+    encoder->finish();
+    bout->finish();
     return 0;
 }
--- README.md
+++ README.md
@@ -190,3 +190,34 @@ ios::trunc：　  //如果文件存在，把文件长度设为0
 |   double    |   64=8字节   |  15~16   |  -1.7 * 10^-308^ ~ 1.7 * 10^308^  |
 | long double |  128=16字节  |  18~19   | -1.2 * 10^-4932^ ~ 1.2 * 10^4932^ |
 
+二、cin.ignore(a,ch)
+
+  从输入流（cin）中提取字符，提取的字符被忽略（ignore），不被使用。每抛弃一个字符，它都要计数和比较字符：如果计数值达到a或者被抛弃的字符是ch，则cin.ignore()函数执行终止；否则，它继续等待。
+
+它的一个常用功能就是用来清除以回车结束的输入缓冲区的内容，消除上一次输入对下一次输入的影响。
+
+比如可以这么用：
+
+cin.ignore(1024,'\n')，通常把第一个参数设置得足够大，这样实际上总是只有第二个参数'\n'起作用，所以这一句就是把回车（包括回车）之前的所以字符从输入缓冲（流）中清除出去。
+
+
+
+三、cin.sync()和cin.ignor()的比较
+
+  sync()的作用就是清除输入缓冲区。成功时返回0，失败时badbit会置位，函数返回-1.
+另外，对于绑定了输出的输入流，调用sync()，还会刷新输出缓冲区。
+
+ 
+
+但由于程序运行时并不总是知道外部输入的进度，很难控制是不是全部清除输入缓冲区的内容。通常我们有可能只是希望放弃输入缓冲区中的一部分，而不是全部。比如清除掉当前行、或者清除掉行尾的换行符等等。但要是缓冲区中已经有了下一行的内容，这部分可能是我们想保留的。这个时候最好不要用sync()。可以考虑用ignore函数代替。
+`cin.ignore(numeric_limits<std::streamsize>::max(),'/n');//清除当前行`
+`cin.ignore(numeric_limits<std::streamsize>::max());   //清除cin里所有内容`
+
+不要被长长的名字吓倒，numeric_limits<std::streamsize>::max()不过是climits头文件定义的流使用的最大值，你也可以用一个足够大的整数代替它。
+
+使用ignore显然能比sync()更精确控制缓冲区。（1）
+
+
+
+[nayuki/Reference-arithmetic-coding: Clear implementation of arithmetic coding for educational purposes in Java, Python, C++. (github.com)](https://github.com/nayuki/Reference-arithmetic-coding)
+
